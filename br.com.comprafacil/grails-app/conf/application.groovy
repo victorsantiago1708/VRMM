c1d744f25402a9ca4a000fa6f1fec95a0610448c
Marcos <marcos.costa.sousa@hotmail.com>
2017-05-14 18:29:36 -0300

    fabricante.

--- br.com.comprafacil/grails-app/conf/application.groovy
+++ br.com.comprafacil/grails-app/conf/application.groovy
@@ -18,7 +18,8 @@ grails.plugin.springsecurity.controllerAnnotations.staticRules = [
 	[pattern: '/**/js/**',       access: ['permitAll']],
 	[pattern: '/**/css/**',      access: ['permitAll']],
 	[pattern: '/**/images/**',   access: ['permitAll']],
-	[pattern: '/**/favicon.ico', access: ['permitAll']]
+	[pattern: '/**/favicon.ico', access: ['permitAll']],
+	[pattern: '/fabricante/**',  access: ['permitAll']],
 ]
 
 grails.plugin.springsecurity.filterChain.chainMap = [
